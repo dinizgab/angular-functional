7c4aaa6f6e34c5a25f08a2b253d39ce3b4ea121c
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-08-21 22:34:29 -0300

    feat: using observable to change state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, inject, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import Issue from '../types/Issue';
+import { IssueComponent } from './issue/issue.component';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable(
   { providedIn: 'root' }
@@ -12,28 +15,22 @@ export class IssuesService {
 
   constructor(private http: HttpClient) { }
 
-  getIssues(): void {
-    this.http.get<Issue[]>(this.apiUrl).subscribe(data => {
-      data.forEach(item => {
-        let parsedIssue: Issue = {
-          id: item.id,
-          title: item.title,
-          body: item.body,
-          state: item.state,
-          number: item.number,
-          labels: item.labels,
-          user: {
-            login: item.user.login
-          },
-          created_at: item.created_at,
-          url: item.url
-        }
-
-        this.issues.push(parsedIssue);
-      })
-    });
-
-    console.log(this.issues);
+  getIssues(): Observable<Issue[]> {
+    return this.http.get<Issue[]>(this.apiUrl).pipe(
+      map((data: Issue[]) => data.map((item) => ({
+        id: item.id,
+        title: item.title,
+        body: item.body,
+        state: item.state,
+        number: item.number,
+        labels: item.labels,
+        user: {
+          login: item.user.login,
+        },
+        created_at: item.created_at,
+        url: item.url,
+      })))
+    );
   }
 }
 
@@ -41,18 +38,28 @@ export class IssuesService {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IssueComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Gabriel';
+  selectedIssue: Issue | undefined;
+  issues$: Observable<Issue[]>;
 
   http = inject(HttpClient);
 
-  constructor(private issuesService: IssuesService) { }
+  constructor(private issuesService: IssuesService) {
+    this.issues$ = this.issuesService.getIssues();
+  }
 
   ngOnInit() {
     this.issuesService.getIssues();
+
+    this.issues$.subscribe((issues) => {
+      if (issues.length > 0) {
+        this.selectedIssue = issues[0];
+      }
+    })
   }
 }
